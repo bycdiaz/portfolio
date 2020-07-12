5973cf52fe745278f6b1eea28f2d5edf546811e0
Carlos Diaz <cdiaz86@gmail.com>
2020-07-11 20:30:49 -0400

    Updates

---
+++ precache-manifest.4d56f2e51170252cf9ad6e4a2db05a0a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fc453f4cea1ae0191dad75e68ee646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847fe4b6e0f8a851a823",
+    "url": "/static/css/main.10acda99.chunk.css"
+  },
+  {
+    "revision": "8180f6b83b2fbbd4950e",
+    "url": "/static/js/2.ef7aae91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef7aae91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847fe4b6e0f8a851a823",
+    "url": "/static/js/main.931d8d36.chunk.js"
+  },
+  {
+    "revision": "ddbdf6a5812a0246912c",
+    "url": "/static/js/runtime-main.b73410b2.js"
+  },
+  {
+    "revision": "4c572fbe0149320b2b324b58173fdead",
+    "url": "/static/media/configzme.4c572fbe.png"
+  },
+  {
+    "revision": "a41c32a5e33ad50ba8ede9cfc9aba251",
+    "url": "/static/media/grotonfd-toggle.a41c32a5.gif"
+  },
+  {
+    "revision": "245a0c598c9232fbf3012ded3336f4c4",
+    "url": "/static/media/html.245a0c59.png"
+  },
+  {
+    "revision": "54b6e4c86f07a054b18e1a232e7de18a",
+    "url": "/static/media/prereqcheck.54b6e4c8.png"
+  },
+  {
+    "revision": "9c3d01720d42944fa93fdcdcd316b782",
+    "url": "/static/media/pug.9c3d0172.svg"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/static/media/react.82a318cc.png"
+  },
+  {
+    "revision": "37383f4c2ec313b32db7988542216388",
+    "url": "/static/media/ruby.37383f4c.png"
+  },
+  {
+    "revision": "e22cf4ed5c732116c98dafeb720a69db",
+    "url": "/static/media/sinatra.e22cf4ed.png"
+  },
+  {
+    "revision": "d443c086950534cd2a77a057c628265e",
+    "url": "/static/media/vue.d443c086.svg"
+  }
+]);
